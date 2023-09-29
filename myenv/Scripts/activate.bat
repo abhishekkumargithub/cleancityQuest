249b81c85fb9ca2f921a79f250790ccc0fba3aa0
abhishekkumargithub <abhishek.kumar2021d@vitstudent.ac.in>
2023-09-29 20:50:32 +0530

    made the profile page responsive

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ABHISHEK KUMAR\OneDrive - vit.ac.in\Desktop\review\cleancityQuest\myenv
+set VIRTUAL_ENV=C:\Users\ABHISHEK KUMAR\OneDrive - vit.ac.in\Desktop\profileres\cleancityQuest\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
